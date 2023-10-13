9c34980a2e2af0cc951a9b440e83e3ec8b3a7e01
kevel <keveltiah@gmail.com>
2023-10-13 11:45:58 +0100

    gunicorn

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -22,10 +22,12 @@ pip install -e /openimis-be-report-cs_py
 pip install -e /openimis-be-policy_py
 pip install -e /openimis-be-insuree_py
 pip install -e /openimis-be-medical_py
+pip install gunicorn
 cd /openimis-be/openIMIS/
-python manage.py runserver 0.0.0.0:8000
+gunicorn -b 0.0.0.0 -w 4 openIMIS.wsgi
+#python manage.py runserver 0.0.0.0:8000
 
-while :; do sleep 10; done
+#while :; do sleep 10; done
 
 show_help() {
   echo """
@@ -34,11 +36,20 @@ show_help() {
 
   start            : start django
   worker           : start Celery worker
-
+  start_asgi       : use daphne -b ASGI_IP:WSGI_PORT -p SERVER_PORT  ASGI_APPLICATION
+  start_wsgi       : use gunicorn -b 0.0.0.0 -w 4 openIMIS.wsgi
   manage           : run django manage.py
   eval             : eval shell command
   bash             : run bash
   """
+}
+
+init(){
+  if [ "${DJANGO_MIGRATE,,}" == "true" ] || [ -z "$SCHEDULER_AUTOSTART" ]; then
+        echo "Migrating..."
+        python manage.py migrate
+        export SCHEDULER_AUTOSTART=True
+  fi
 }
 
 #export PYTHONPATH="/opt/app:$PYTHONPATH"
@@ -48,25 +59,35 @@ fi
 
 case "$1" in
   "start" )
-    if [ "$SITE_ROOT" == "api" ] 
-    then
-      echo "Migrating..."
-      python manage.py migrate
-    fi
+    init
     echo "Starting Django..."
-    SCHEDULER_AUTOSTART=True python server.py
+    python server.py
   ;;
   "start_asgi" )
+    init
     echo "Starting Django ASGI..."
     def_ip='0.0.0.0'
     def_port='8000'
     def_app='openIMIS.asgi:application'
-
     SERVER_IP="${ASGI_IP:-$def_ip}"
     SERVER_PORT="${ASGI_PORT:-$def_port}"
     SERVER_APPLICATION="${ASGI_APPLICATION:-$def_app}"
 
     daphne -b "$SERVER_IP" -p "$SERVER_PORT" "$SERVER_APPLICATION"
+  ;;
+  "start_wsgi" )
+    init
+    echo "Starting Django WSGI..."
+    def_ip='0.0.0.0'
+    def_port='8000'
+    def_app='openIMIS.wsgi'
+
+    SERVER_IP="${WSGI_IP:-$def_ip}"
+    SERVER_PORT="${WSGI_PORT:-$def_port}"
+    SERVER_APPLICATION="${WSGI_APPLICATION:-$def_app}"
+    SERVER_WORKERS="${WSGI_WORKERS:-4}"
+
+    gunicorn -b "$SERVER_IP:$SERVER_PORT" -w $SERVER_WORKERS "$SERVER_APPLICATION"
   ;;
   "worker" )
     echo "Starting Celery with url ${CELERY_BROKER_URL} ${DB_NAME}..."
