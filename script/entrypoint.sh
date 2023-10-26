00b2a85aa92dbaa8ea3d9a24f3987d612b7b8a9e
kevel <keveltiah@gmail.com>
2023-10-26 16:20:00 +0100

    version fhir locale du a la longeur du chfid

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -22,6 +22,7 @@ pip install -e /openimis-be-report-cs_py
 pip install -e /openimis-be-policy_py
 pip install -e /openimis-be-insuree_py
 pip install -e /openimis-be-medical_py
+pip install -e /openimis-be-api_fhir_r4_py
 pip install pydantic==1.10.0
 pip install gunicorn
 cd /openimis-be/openIMIS/
