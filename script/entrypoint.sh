23a6e239ff2531c9365bad3ef0104828f43aa444
kevel choumene <k.choumene@cloudfacile.com>
2025-01-15 14:35:13 +0100

    feat: Ajustement comme sur le serveur test

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -21,9 +21,9 @@ pip install -e /openimis-be-report-cs_py
 #pip install -e /openimis-be-tools_py
 #pip install -e /openimis-be-idps_py
 #cp /openimis-be/script/fhirtypes.py /usr/local/lib/python3.10/site-packages/fhir/resources/
-pip install pydantic==1.10.0
-pip install gunicorn
-pip install django-debug-toolbar
+# pip install pydantic==1.10.0
+# pip install gunicorn
+# pip install django-debug-toolbar
 cd /openimis-be/openIMIS/
 #python manage.py runserver 0.0.0.0:8000
 
