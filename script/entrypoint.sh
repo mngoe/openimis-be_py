7ead930b2b465043c8d194a841b9c01ffb82d987
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-08-07 16:40:32 +0100

    Fix:Update entrypoint

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -26,6 +26,7 @@ pip install pydantic==1.10.0
 pip install gunicorn
 pip install openpyxl
 pip install django-debug-toolbar
+pip install -r requirements.txt
 cd /openimis-be/openIMIS/
 #python server.py 0.0.0.0:8000
 
