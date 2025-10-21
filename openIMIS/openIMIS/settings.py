30ecaaf0b06d3c36c586a3f7945d36f8bd567acd
Le-positif-clinton <105806913+Le-positif-clinton@users.noreply.github.com>
2025-10-21 12:11:28 +0100

    Update settings.py

--- openIMIS/openIMIS/settings.py
+++ openIMIS/openIMIS/settings.py
@@ -485,7 +485,7 @@ SCHEDULER_JOBS = [
     {
         "method": "policy.tasks.get_policies_for_renewal",
         "args": ["cron"],
-        "kwargs": {"id": "openimis_renewal_batch", "hour": 8, "minute": 30, "replace_existing": True},
+        "kwargs": {"id": "openimis_renewal_batch", "hour": 11, "minute": 30, "replace_existing": True},
     },
     # {
     #     "method": "policy_notification.tasks.send_notification_messages",
