5ce274a9f1ce558c3b5abb8cc3a7db4359ece7cb
Maxime NGOE <maxime.ngoe@gmail.com>
2026-07-08 06:07:45 +0530

    Fix entrypoint.sh : Django Version
    python modules-requirements.py openimis.json > modules-requirements.txt
    pip uninstall -r modules-requirements.txt -y
    pip install -r modules-requirements.txt -c requirements.txt
    pip install -r sentry-requirements.txt
    pip install -e /openimis-be-program_py -c requirements.txt
    pip install -e /openimis-be-cs_py -c requirements.txt
    pip install -e /openimis-be-csu_py -c requirements.txt
    pip install -e /openimis-be-report-csu_py -c requirements.txt
    pip install -e /openimis-be-report-cs_py -c requirements.txt

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -4,17 +4,22 @@ set -e
 cd /openimis-be/
 python modules-requirements.py openimis.json > modules-requirements.txt
 pip uninstall -r modules-requirements.txt -y
-pip install -r modules-requirements.txt
+pip install -r modules-requirements.txt -c requirements.txt
 pip install -r sentry-requirements.txt
+pip install -e /openimis-be-program_py -c requirements.txt
+pip install -e /openimis-be-cs_py -c requirements.txt
+pip install -e /openimis-be-csu_py -c requirements.txt
+pip install -e /openimis-be-report-csu_py -c requirements.txt
+pip install -e /openimis-be-report-cs_py -c requirements.txt
 #pip install -e /openimis-be-core_py
-pip install -e /openimis-be-program_py
-pip install -e /openimis-be-cs_py
-pip install -e /openimis-be-csu_py
+#pip install -e /openimis-be-program_py
+#pip install -e /openimis-be-cs_py
+#pip install -e /openimis-be-csu_py
 #pip install -e /openimis-be-claim_py
 #pip install -e /openimis-be-location_py
 #pip install -e /openimis-be-product_py
-pip install -e /openimis-be-report-csu_py
-pip install -e /openimis-be-report-cs_py
+#pip install -e /openimis-be-report-csu_py
+#pip install -e /openimis-be-report-cs_py
 #pip install -e /openimis-be-policy_py
 #pip install -e /openimis-be-insuree_py
 #pip install -e /openimis-be-medical_py
